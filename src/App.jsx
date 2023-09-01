0c3853d25f763c1936162e8e3196f9e618029d1a
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-09-01 13:47:12 +0700

    ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import Header from './component/Header';
 import Profile from './component/Profile';
+import "./App.css";
 
 function App() {
   const [likes, setLikes] = useState(0);
@@ -9,22 +10,30 @@ function App() {
   function handleClick() {
     setLikes(likes + 1);
   }
+
+  const age = 19;
+  const isGreen = true;
+
   return (
-    <>
-      <Header />
-      <Profile /><br />
-      <User name= "Ghulam" title="Frontend Developer" email="ghulam@gmail.com"/>
-      <User name= "Ammar" title="Backend Developer" email="ammar@gmail.com"/>
-      <button onClick={handleClick}>Like ({likes})</button>
-      {/*<Header author="Sandika Galih"/>*/}
-    </>
+    <div className='App'>
+      {age >= 18 ? <h1>over age</h1> : <h1>under age</h1>}
+      <h1 style={{color: isGreen ? "green" : "red"}}>this is color</h1>
+    </div>
+    // <>
+    //   <Header />
+    //   <Profile /><br />
+    //   <User name= "Ghulam" title="Frontend Developer" email="ghulam@gmail.com"/>
+    //   <User name= "Ammar" title="Backend Developer" email="ammar@gmail.com"/>
+    //   <button onClick={handleClick}>Like ({likes})</button>
+    //   <Header author="Sandika Galih"/>
+    // </>
   );
 }
 
 const User = (props) => {
   return (
     <>
-      <h1>{props.name}</h1>
+      <h1 className='name'>{props.name}</h1>
       <h1>{props.title}</h1>
       <h1>{props.email}</h1>
     </>
