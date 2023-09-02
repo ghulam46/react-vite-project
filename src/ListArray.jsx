e56a2c560e3aed5cc931683f3587869c3fff0b34
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-09-02 11:46:58 +0700

    rendering lists

--- src/ListArray.jsx
+++ src/ListArray.jsx
@@ -2,12 +2,25 @@ import "./App.css";
 
 export default function ListArray() {
 
-    const names = ["Pedro", "Jake", "Jessica", "Mike"];
+    // const names = ["Pedro", "Jake", "Jessica", "Mike"];
+    const users = [
+        { name: "Pedro", age: 22 },
+        { name: "Jake", age: 25 },
+        { name: "Mike", age: 20 }
+    ];
 
     return(
         <div className="App">
-            {names.map((name, key) => {
+            {/* {names.map((name, key) => {
                 return <h1 key={key}>{name}</h1>
+            })} */}
+            
+            {users.map((user, key) => {
+                return (
+                    <div key={key}>
+                        {user.name} {user.age}
+                    </div>
+                );
             })}
         </div>
     );
