53056fb18ab693802565470068bb529eefb17d1e
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-09-02 12:22:43 +0700

    rendering list part 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ListArray from './ListArray.jsx'
+import ListArray from './rendering lists/ListArray.jsx'
+import AppRl from '../src/rendering lists/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <ListArray />
+    {/* <ListArray /> */}
+    <AppRl />
   </React.StrictMode>,
 )
---
+++ src/rendering lists/App.jsx
@@ -0,0 +1,20 @@
+import { people } from '../rendering lists/data.jsx';
+import { getImageUrl } from '../rendering lists/utils.jsx';
+import '../../src/App.css';
+
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
