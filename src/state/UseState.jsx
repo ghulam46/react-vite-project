5ed0f78b69a45b301e8f636c5851802bdb69cc51
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-09-07 09:28:47 +0700

    add opacity

--- src/state/UseState.jsx
+++ src/state/UseState.jsx
@@ -11,6 +11,9 @@ export default function UseState() {
     const decreaseProduct = () => {
         (productInStock > 0 ) && setProductInStock(productInStock-1);
     }
+    const resetProduct = () => {
+        setProductInStock(0);
+    }
 
     const pis = <h1 className="text-2xl text-white bg-green-500">Product In Stock</h1>;
     const pos = <h1 className="text-2xl text-white bg-red-500">Product Out Of Stock</h1>;
@@ -19,9 +22,10 @@ export default function UseState() {
         <div className="App">
             <div className="p-10">
                 <h1 className="text-3xl text-blue-800 mb-3">Product Stock</h1>
-                <button className="border-2 border-red-500 px-10 mr-3" onClick={decreaseProduct}>-</button>
+                {productInStock === 0 ? <button className="border-2 border-red-500 px-10 mr-3 opacity-25" onClick={decreaseProduct}>-</button> : <button className="border-2 border-red-500 px-10 mr-3" onClick={decreaseProduct}>-</button>}
                 {productInStock}
                 <button className="border-2 border-green-500 px-10 ml-3 mb-5" onClick={increaseProduct}>+</button><br/>
+                <button className="border-2 border-green-500 px-10 ml-3 mb-5" onClick={resetProduct}>Reset</button><br/>
                 {productInStock > 0 ? pis : pos}
             </div>
         </div>
