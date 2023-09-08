b92266e8f1088eb23db3dbcb68dd1bacf2bcb0bf
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-09-08 14:58:09 +0700

    useState : handle previous button

--- src/state/ComponentMemory.jsx
+++ src/state/ComponentMemory.jsx
@@ -6,9 +6,14 @@ export default function ComponentMemory() {
     const [showText, setShowText] = useState(false);
 
     let sculpture = sculptureList[index];
+    let hasNext = index < sculptureList.length - 1;
+    let hasPrev = index > 0;
 
-    function handleClick() {
-        setIndex((index + 1) % sculptureList.length)
+    function handleNextClick() {
+        (hasNext) && setIndex(index + 1)
+    }
+    function handlePreviousClick() {
+        (hasPrev) && setIndex(index - 1)
     }
 
     function handleMoreClick() {
@@ -17,11 +22,20 @@ export default function ComponentMemory() {
     
     return(
         <div className='p-10'>
-            <button className='btn btn-outline btn-primary mb-2' onClick={handleClick}>Next</button>
+            <button className='btn btn-outline btn-primary mb-2' 
+                onClick={handlePreviousClick} 
+                disabled={!hasPrev}>
+                Previous
+            </button>
+            <button className='btn btn-outline btn-primary mb-2' 
+                onClick={handleNextClick} 
+                disabled={!hasNext}>
+                Next
+            </button>
             <h2 className='text-4xl mb-3'>
                 <i>{sculpture.name}</i> 
                 <h2 className='text-sm'>
-                    {''}by {sculpture.artist}
+                    by {sculpture.artist}
                 </h2>
             </h2>
             <h3 className='mb-2'>({index + 1} of {sculptureList.length})</h3>
