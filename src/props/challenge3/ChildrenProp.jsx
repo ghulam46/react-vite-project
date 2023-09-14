5ecd04ed977740c7e99785aa108aa999628b1d19
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-09-14 15:49:43 +0700

    [react.dev - props] challenge - update Passing JSX in a children prop

--- src/props/challenge3/ChildrenProp.jsx
+++ src/props/challenge3/ChildrenProp.jsx
@@ -1,7 +1,17 @@
+/* eslint-disable react/prop-types */
+function Card({children}) {
+    return(
+        <div className="border-2 w-[550px] rounded-xl text-center p-5">
+            {children}
+        </div>
+    );
+}
+
 export default function ChildrenProp() {
+
     return(
         <div className="mx-auto card card-body shadow-xl w-1/2">
-            <div className="border-2 w-20 rounded-xl p-3">
+            <Card>
                 <h1>Photo</h1>
                 <img 
                     className="avatar rounded-xl"
@@ -10,11 +20,11 @@ export default function ChildrenProp() {
                     width={70}
                     height={70}
                 />
-            </div>
-            <div className="border-2 w-[550px] rounded-xl text-center p-5">
+            </Card>
+            <Card>
                 <h1>About</h1>
                 <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-            </div>
+            </Card>
         </div>
     );
 }
