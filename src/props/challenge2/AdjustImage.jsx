2088841feb12a1147aff916ae8b33056d670b2b8
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-09-14 14:50:29 +0700

    [react.dev - props] challenge Adjust the image size based on a prop with className

--- src/props/challenge2/AdjustImage.jsx
+++ src/props/challenge2/AdjustImage.jsx
@@ -3,13 +3,17 @@ import { getImageUrl } from '../challenge2/utils.jsx';
  
 function Avatar({ person, size }) {
     let imgThumbnail = 's';
+    let imgRounded = '';
     if(size > 90) {
         imgThumbnail = 'b';
+        imgRounded = 'avatar rounded-full';
+    } else {
+        imgRounded = 'avatar rounded-lg';
     }
  return (
     <div className='text-center py-5'>
         <img
-            className="avatar rounded-full"
+            className={imgRounded}
             src={getImageUrl(person, imgThumbnail)}
             alt={person.name}
             width={size}
